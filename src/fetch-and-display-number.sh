fa2d85fcdd905b08dfc1b4bdc6dbbc9c63f7805c
Dr Nic Williams <drnicwilliams@gmail.com>
2020-02-25 15:40:12 +1000

    new line

--- src/fetch-and-display-number.sh
+++ src/fetch-and-display-number.sh
@@ -8,6 +8,7 @@ if [[ "$action" == "collect" ]]; then
 
   for((i=0;i<parallel_numbers;i++)); do
     ( set -x ; buildkite-agent meta-data get "generated-number::$i" )
+    echo
   done
 else
   ( set -x ; buildkite-agent meta-data get generated-number )
