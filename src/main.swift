465f0e1072571da056e9b589751fcb761bace359
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2022-04-25 16:19:07 -0700

    Create main.swift

---
+++ src/main.swift
@@ -0,0 +1,18 @@
+#!/usr/bin/swift 
+
+import Foundation 
+class MythicalBeast { 
+    func whatsMyName() { 
+        print("I don't know what I am, but I'm the stuff of legends.") 
+    } 
+} 
+
+class Kraken: MythicalBeast { 
+    override func whatsMyName() { 
+        print("I'm the Kraken, yo!") 
+    } 
+} 
+
+//can't use the Kraken class until after the declaration 
+let kraken = Kraken()
+kraken.whatsMyName()
