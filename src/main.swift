fc002ff144cfa5ae0b6688a961a034283d038b23
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2022-04-25 17:05:19 -0700

    Update main.swift

--- src/main.swift
+++ src/main.swift
@@ -5,15 +5,6 @@ class MythicalBeast {
     func whatsMyName() { 
         print("I don't know what I am, but I'm the stuff of legends.") 
     } 
-} 
-
-class Kraken: MythicalBeast { 
-    override func whatsMyName() { 
-        print("I'm the Kraken, yo!") 
-        fputs("I'm the Kraken, yo!", stdout)
-    } 
-} 
-
-//can't use the Kraken class until after the declaration 
+}  
 let mb = MythicalBeast()
 mb.whatsMyName()
