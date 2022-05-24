ade92adda2556ba39d3106a668de2db5d26d22b6
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2022-05-23 18:45:36 -0700

    Update env.sh

--- src/env.sh
+++ src/env.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+set -euo pipefail
+export ESCAPE_PROD=".3!)@4#pis+\"Gj)qz*YF@N;nz;NR5_7JYN')x?H#{fes(f98!GSf)uoY?cv'9m"'`'"D"
+export ESCAPE_TEST="#&K+_)Jk[4*L#Ikb'8W^f%=!L6}&TfuS?v8BN%MlkvS>-F3uk'Op,=)*B)^2tu?D"
+export ESCAPE_NEW_WAY=".3!)@4#pis+\"Gj)qz*YF@N;nz;NR5_7JYN')x?H#{fes(f98!GSf)uoY?cv'9m\`D"
+echo ESCAPE_PROD
+echo ESCAPE_TEST
+echo ESCAPE_NEW_WAY
