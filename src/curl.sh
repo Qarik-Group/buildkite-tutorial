0098bd21935c8741268fe6d3c6e196891822a641
Suma Naidu <79238214+nsuma8989@users.noreply.github.com>
2023-03-07 18:07:52 -0800

    Update curl.sh

--- src/curl.sh
+++ src/curl.sh
@@ -1,2 +1,3 @@
 curl https://graphql.buildkite.com/v1 \
+-H "Authorization: Bearer $TOKEN" \
   -d '{"query":"query { build(slug: \"sumatestorg/artifact-test/136\") { jobs(first: 500, step: { key: \"knapsack\" }) { edges { node { ... on JobTypeCommand { artifacts(first: 500) { edges { node { downloadURL path uuid } } } } } } } }}"}'
